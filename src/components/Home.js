343263d4eba7a590bec4699153f787b84285d3b7
Thomas de Groot <degroot12@gmail.com>
2021-03-19 18:41:01 +0100

    added first project

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,13 @@ export default function Home() {
         <div>
             <h1>Thomas de Groot</h1>
             <h2>My Projects:</h2>
+            <div className='super-max'>
+            <h3>Super-Max</h3>
+            <img src='./supermax-game-normal.png'></img>
+            <p>Super-Max is a Canvas game where you have to avoid obstacles and collect points.
+                If your score is higher than Lewis Hamilton's you win!
+                The game has argualy the greatest soundtrack of all time</p>
+            </div>
         </div>
     )
 }
