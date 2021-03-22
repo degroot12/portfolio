4bee077fa6aa3a24411780e0cceeacd576f6ff90
Thomas de Groot <degroot12@gmail.com>
2021-03-22 17:23:53 +0100

    added links to projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,21 +8,21 @@ export default function Home() {
             <h2>My Projects:</h2>
             </a>
             <div className='game-summary'>
-            <h3>Super-Max</h3>
+            <h3><a href='https://degroot12.github.io/Super-Max' target='_blank'>Super-Max</a></h3>
             <img src='./supermax-game-normal.png'></img>
             <p>Super-Max is a Canvas game where you have to avoid obstacles and collect points.
                 If your score is higher than Lewis Hamilton's you win!
                 The game has argualy the greatest soundtrack of all time</p>
             </div>
             <div className='game-summary'>
-            <h3>Click-n-Meal</h3>
+            <h3><a href='https://click-n-meal.herokuapp.com' target='_blank'>Click-n-Meal</a></h3>
             <img src='./Click-n-meal-preview.png'/>
             <p>Click-n-Meal is an easy way to find the recipes to your liking. You can filter the recipes 
                 based on the price, prepare time or wether it is vegan or not.
                 You can also add you own recipes to the database</p>
             </div>
             <div className='game-summary'>
-                <h3>Band-Square</h3>
+                <h3><a href='https://band-square.herokuapp.com/' target='_blank'>Band-Square</a></h3>
                 <img src='./band-square.png'/>
                 <p>Band-Square is a platform where musicians can connect with other muscians. 
                    If you have a band you can look for other people to join your band, of look for venues where you 
